55b64724210985f50b482e16398b7a940d699d66
Jananath Banuka Jayarathna <32482769+iamdempa@users.noreply.github.com>
2019-07-16 19:04:03 +0530

    Commit

--- includes/header.php
+++ includes/header.php
@@ -26,13 +26,13 @@ if (session_status() == PHP_SESSION_NONE) {
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="../index.php"><i class='fa fa-home'> Home <span class="sr-only">(current)</i></span></a>
+                    <a class="nav-link" href="../index.php"><i class='fa fa-home'>  <span class="sr-only">(current)</i></span> Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#"><i class='fa fa-user'> About us</i></a>
+                    <a class="nav-link" href="#"><i class='fa fa-user'> </i> About us</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#"><i class='fa fa-phone'> Contact us</i></a> 
+                    <a class="nav-link" href="#"><i class='fa fa-phone'> </i> Contact us</a> 
                 </li>
                 
                 <form class="form-inline mt-2 mt-md-0" action="includes/search-ads-inc.php" method="POST">
