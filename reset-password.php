f954cd1b251e666ece15572ea3e4b6a2a0b8aa43
Jananath Banuka Jayarathna <32482769+iamdempa@users.noreply.github.com>
2019-07-30 11:46:32 +0530

    init

--- reset-password.php
+++ reset-password.php
@@ -3,7 +3,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
     if (isset($_SESSION['username'])) {
         header("Location: index.php");
-        echo 'hi';
+        exit();
     }
 } 
 ?>
