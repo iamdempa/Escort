0fa20ac520c0c4728c8809673e017cf4e7bf3e25
Jananath Banuka Jayarathna <32482769+iamdempa@users.noreply.github.com>
2019-08-01 17:28:25 +0530

    init

--- AdminDashboard/includesadmin/javascript-sheets.php
+++ AdminDashboard/includesadmin/javascript-sheets.php
@@ -11,5 +11,7 @@
 <script src="jsadmin/jquery.vmap.sampledata.js"></script>
 <script src="jsadmin/jquery.vmap.world.js"></script>
 
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<!--<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>-->
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" ></script>
