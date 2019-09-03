da8f1f751f46550ee323a3bf1bf0e9a865399d3c
Jananath Banuka Jayarathna <32482769+iamdempa@users.noreply.github.com>
2019-09-03 20:00:33 +0530

    init

--- index.php
+++ index.php
@@ -454,13 +454,14 @@
         <!-- Bootstrap core JavaScript
         ================================================== -->
         <!-- Placed at the end of the document so the pages load faster -->
+        <script src="js/main/jquery.js"></script>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script>window.jQuery || document.write('<script src="js/main/jquery-slim.min.js"><\/script>')</script>
         <script src="js/main/popper.min.js"></script>
         <script src="js/main/bootstrap.js"></script>
         <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
         <script src="js/main/holder.min.js"></script>
-        <script src="js/main/jquery.js"></script>
+        
 
         <!--Back to top-->
         <script type="text/javascript">
