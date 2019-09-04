f22faa81667d42ffda946c30238e4e24ccbf9e37
iamdempa <jbjayarathna@gmail.com>
2019-09-04 16:13:16 +0530

    changed database connections to a main database file

--- AdminDashboard/includesadmin/dbConnection.php
+++ AdminDashboard/includesadmin/dbConnection.php
@@ -1,15 +1,3 @@
 <?php
 
-$dbServerName = "localhost";
-$dbUserName = "root";
-$dbPassword = "root123";
-$dbName = "escort";
-
-$conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
-
-if(!$conn){
-    echo 'Failed to connect';
-} else {
-//    echo 'Success';
-}
-
+include_once '../../mainDatabase.php';
